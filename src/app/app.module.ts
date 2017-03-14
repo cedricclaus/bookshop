2f937723e1771261b2f942f82b7721acfe40007c
cedricclaus <cedric.claus@gmail.com>
2017-03-14 11:05:54 +0100

    generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CatalogRootComponent } from './catalog/catalog-root/catalog-root.component';
+import { CatalogListComponent } from './catalog/catalog-list/catalog-list.component';
+import { CatalogBookComponent } from './catalog/catalog-book/catalog-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogRootComponent,
+    CatalogListComponent,
+    CatalogBookComponent
   ],
   imports: [
     BrowserModule,
