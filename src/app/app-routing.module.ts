746285c2194a688d92dbb25c5a905f409a116872
cedricclaus <cedric.claus@gmail.com>
2017-03-14 14:00:37 +0100

    add routing catalog

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+/**
+ * Created by ccc on 14-Mar-17.
+ */
+import {NgModule} from "@angular/core";
+import {RouterModule} from "@angular/router";
+import {CatalogRootComponent} from "./catalog/catalog-root/catalog-root.component";
+
+const routes = [
+  {path: 'catalog', component: CatalogRootComponent},
+  {path: '', redirectTo: 'catalog', pathMatch: 'full'}
+]
+
+@NgModule(
+  {
+    imports: [
+      RouterModule.forRoot(routes)
+    ],
+    exports: [RouterModule]
+  }
+)
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import {CatalogModule} from "./catalog/catalog.module";
 import {ServicesModule} from "./core/services/services.module";
+import {AppRoutingModule} from "./app-routing.module";
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import {ServicesModule} from "./core/services/services.module";
     FormsModule,
     HttpModule,
     CatalogModule,
-    ServicesModule
+    ServicesModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
