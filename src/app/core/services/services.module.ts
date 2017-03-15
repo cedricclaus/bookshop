c9685e8fcaf47d25cf590fb4ecc3baabd854dd91
cedricclaus <cedric.claus@gmail.com>
2017-03-15 08:46:46 +0100

    generation for cart

--- src/app/core/services/services.module.ts
+++ src/app/core/services/services.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {CatalogService} from "./catalog.service";
+import {CartService} from "../service/cart.service";
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  providers : [CatalogService],
+  providers : [CatalogService, CartService],
   declarations: []
 })
 export class ServicesModule { }
