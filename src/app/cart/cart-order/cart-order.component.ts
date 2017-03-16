dbfc97d6630dbd8b02c39bf37caa1190b75e92ff
cedricclaus <cedric.claus@gmail.com>
2017-03-16 15:30:36 +0100

    add menu item and css

--- src/app/cart/cart-order/cart-order.component.ts
+++ src/app/cart/cart-order/cart-order.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NgForm, NgModel} from "@angular/forms";
 
 @Component({
   selector: 'app-cart-order',
@@ -7,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartOrderComponent implements OnInit {
 
+  @ViewChild('form') form : NgForm;
+  @ViewChild('last') last : NgModel;
+
   constructor() { }
+
+  data = {
+    identity : {},
+    shipping:{},
+    billing:{}
+  }
+
+  log(){
+    console.log(this.form);
+    console.log(this.last);
+  }
 
   ngOnInit() {
   }
