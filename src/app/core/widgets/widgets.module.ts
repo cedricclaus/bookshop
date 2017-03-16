1394de4ea11272681b2725d961eef7650ea3c867
cedricclaus <cedric.claus@gmail.com>
2017-03-16 21:05:10 +0100

    Create component for validation

--- src/app/core/widgets/widgets.module.ts
+++ src/app/core/widgets/widgets.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartSummaryComponent } from './cart-summary/cart-summary.component';
+import { ErrorComponent } from './error/error.component';
+import { ControlComponent } from './control/control.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  exports:[CartSummaryComponent],
-  declarations: [CartSummaryComponent]
+  exports:[CartSummaryComponent,ErrorComponent,ControlComponent],
+  declarations: [CartSummaryComponent, ErrorComponent, ControlComponent]
 })
 export class WidgetsModule { }
